ff9a6bb85a7cb6aa81c039de33be37ebdd52ae20
Joaquin Yanez <joaquin.ayanezm@gmail.com>
2021-05-13 22:23:17 -0400

    First commit

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract Marketplace is Initializable, OwnableUpgradeable {
+    function initialize() internal initializer {
+        OwnableUpgradeable.__Ownable_init();
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,38 +2,48 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-require("dotenv").config();
-require("@nomiclabs/hardhat-truffle5");
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-web3");
+require('dotenv').config();
+require('@nomiclabs/hardhat-etherscan');
+require('@nomiclabs/hardhat-waffle');
+require('hardhat-gas-reporter');
+require('hardhat-log-remover');
+require('@openzeppelin/hardhat-upgrades');
+require('hardhat-deploy');
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-tracer');
 
 module.exports = {
-  networks: {
-    hardhat: {
-      // Uncomment these lines to use mainnet fork
-      forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
-        blockNumber: 11589707,
-      },
+    networks: {
+        hardhat: {
+            // Uncomment these lines to use mainnet fork
+            accounts: {
+                mnemonic: process.env.MNEMONIC,
+                count: 10,
+                accountsBalance: '10000000000000000000000'
+            },
+            forking: {
+                url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+                blockNumber: 11589707
+            }
+        },
+        live: {
+            url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+            accounts: [process.env.MAINNET_PRIVKEY]
+        }
     },
-    live: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [process.env.MAINNET_PRIVKEY],
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API
     },
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API,
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+    solidity: {
+        version: '0.8.4',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
     },
-  },
-  mocha: {
-    timeout: 240000,
-  },
+    mocha: {
+        timeout: 240000
+    }
 };
